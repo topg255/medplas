0c8fcdd776bfddafff46a83e228a38eaf40fc40e
unknown <ahmed.borgi@polytechnicien.tn>
2026-01-16 12:37:32 +0100

    sec commit

--- authMedecin/src/index.js
+++ authMedecin/src/index.js
@@ -9,7 +9,7 @@ import DoctorDashboard from './doctordashboard'
 import OtpVerification from './otp';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from '../redux/store';
+import { store } from './redux/store';
 
 const root = createRoot(document.getElementById('root'));
 
